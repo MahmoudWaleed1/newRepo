89223f90846fe8cc23e6c3819e58015fb6d5dd37
Mahmoud <mahmoudaboelsoaoud@gmail.com>
2024-01-31 14:03:12 +0200

    second

--- 2ndgui.py
+++ 2ndgui.py
@@ -3,6 +3,9 @@ from PyQt5.QtCore import QTimer, QTime
 import datetime
 import os
 
+yawPid_para = [ 6.6, 10 , 1.09]
+pitchPid_para = [4.2 , 6.4 , 0.68]
+
 class Gui(QtWidgets.QMainWindow):
     def __init__(self, parent = None):
         super (Gui, self).__init__(parent)
@@ -31,9 +34,9 @@ class Gui(QtWidgets.QMainWindow):
       
         
         self.yawFlag = False
-        self.yawFlag2 = False
+        self.yawFlag2 = True
         self.pitchFlag = False
-        self.pitchFlag2 = False
+        self.pitchFlag2 = True
 
         self.yaw_kp_slider.setMaximum(2000)
         self.yaw_ki_slider.setMaximum(2000)
@@ -41,6 +44,41 @@ class Gui(QtWidgets.QMainWindow):
         self.pitch_kp_slider.setMaximum(2000)
         self.pitch_ki_slider.setMaximum(2000)
         self.pitch_kd_slider.setMaximum(2000)
+        self.yaw_kp_slider.setSingleStep(1)
+        self.yaw_ki_slider.setSingleStep(1)
+        self.yaw_kd_slider.setSingleStep(1)
+        self.pitch_kp_slider.setSingleStep(1)
+        self.pitch_ki_slider.setSingleStep(1)
+        self.pitch_kd_slider.setSingleStep(1)
+        yaw_kp_ = self.findChild(QtWidgets.QSlider,"yaw_kp_slider")
+        yaw_kp_.valueChanged.connect(lambda value: self.yaw_update_kp (value))
+        yaw_kp_.setValue(round(yawPid_para[0]*5))
+        self.yaw_kp_label = self.findChild(QtWidgets.QLabel,"yaw_kp_label")
+        
+        yaw_ki_ = self.findChild(QtWidgets.QSlider,"yaw_ki_slider")
+        yaw_ki_.valueChanged.connect(lambda value: self.yaw_update_ki (value))
+        yaw_ki_.setValue(round(yawPid_para[1]*5))
+        self.yaw_ki_label = self.findChild(QtWidgets.QLabel,"yaw_ki_label")
+
+        yaw_kd_ = self.findChild(QtWidgets.QSlider,"yaw_kd_slider")
+        yaw_kd_.valueChanged.connect(lambda value: self.yaw_update_kd (value))
+        yaw_kd_.setValue(round(yawPid_para[2]*5))
+        self.yaw_kd_label = self.findChild(QtWidgets.QLabel,"yaw_kd_label")
+        
+        pitch_kp_ = self.findChild(QtWidgets.QSlider,"pitch_kp_slider")
+        pitch_kp_.valueChanged.connect(lambda value: self.pitch_update_kp (value))
+        pitch_kp_.setValue(round(pitchPid_para[0]*5))
+        self.pitch_kp_label = self.findChild(QtWidgets.QLabel,"pitch_kp_label")
+
+        pitch_ki_ = self.findChild(QtWidgets.QSlider,"pitch_ki_slider")
+        pitch_ki_.valueChanged.connect(lambda value: self.pitch_update_ki (value))
+        pitch_ki_.setValue(round(pitchPid_para[1]*5))
+        self.pitch_ki_label = self.findChild(QtWidgets.QLabel,"pitch_ki_label")
+        
+        pitch_kd_ = self.findChild(QtWidgets.QSlider,"pitch_kd_slider")
+        pitch_kd_.valueChanged.connect(lambda value: self.pitch_update_kd (value))
+        pitch_kd_.setValue(round(pitchPid_para[2]*5))
+        self.pitch_kd_label = self.findChild(QtWidgets.QLabel,"pitch_kd_label")
 
 
     def start_trialTimer(self):
@@ -71,6 +109,42 @@ class Gui(QtWidgets.QMainWindow):
             self.stop_trialTimer()
 
     ##########################################################################################################
+    def yaw_update_kp(self, value):
+        self.kp = value / 100   
+        yawPid_para[0] = self.kp
+        self.yaw_kp_label.setText(f"{self.kp:.2f}")
+    
+    def yaw_update_ki(self, value):
+        self.ki = value / 100
+        yawPid_para[1] = self.ki
+
+        self.yaw_ki_label.setText(f"{self.ki:.2f}")
+
+    def yaw_update_kd(self, value):
+        self.kd = value / 100
+        yawPid_para[2] = self.kd    
+
+        self.yaw_kd_label.setText(f"{self.kd:.2f}")        
+
+
+    def pitch_update_kp(self, value):
+        
+        self.kp = value / 100
+        pitchPid_para[0] = self.kp
+        self.pitch_kp_label.setText(f"{self.kp:.2f}")
+
+    def pitch_update_ki(self, value):
+        self.ki = value / 100
+        pitchPid_para[1] = self.ki
+        self.pitch_ki_label.setText(f"{self.ki:.2f}")
+
+    def pitch_update_kd(self, value):
+        self.kd = value / 100
+        pitchPid_para[2] = self.kd
+
+        self.pitch_kd_label.setText(f"{self.kd:.2f}")
+            
+
     def cancel_yaw_control(self):
         if self.yawFlag2:
             if not self.yawFlag:
